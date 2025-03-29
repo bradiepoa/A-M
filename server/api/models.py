685e5788c86395d97655b05177ca9655d25d2878
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2025-03-30 00:51:36 +0300

    adding django API CONFIG

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ("customer", "Customer"),
+        ("vendor", "Vendor"),
+        ("healer", "Healer"),
+        ("doctor", "Doctor"),
+        ("herbalist", "Herbalist"),
+        ("non", "Non"),
+    )
+    username = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="non")
+
+    groups = models.ManyToManyField(Group, related_name="custom_user_groups", blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name="custom_user_permissions", blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["role"]
+
+    def __str__(self):
+        return f"{self.email} ({self.role})"
