668d1d6fbf68b962bbe725dbbad36b402be03b20
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2025-04-03 20:05:09 +0300

    adding admin

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+class VariantInline(admin.TabularInline):
+    model = Variant
+
+
+class VariantItemInline(admin.TabularInline):
+    model = VariantItem
+
+
+
