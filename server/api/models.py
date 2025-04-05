1fa8bdf433dd7220fe112b5a520225ef03d37c1f
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2025-04-05 09:01:30 +0300

    adding accounts urls

--- server/api/models.py
+++ server/api/models.py
@@ -2,45 +2,9 @@ from django.db import models
 from django.utils import timezone
 from ckeditor.fields import RichTextField
 from datetime import datetime, timedelta
-from django.contrib.auth.models import AbstractUser, Group, Permission
 from shortuuidfield import ShortUUIDField
+from accounts.models import *
 import uuid
-
-class User(AbstractUser):
-    ROLE_CHOICES = (
-        ("customer", "Customer"),
-        ("vendor", "Vendor"),
-        ("healer", "Healer"),
-        ("doctor", "Doctor"),
-        ("herbalist", "Herbalist"),
-        ("non", "Non"),
-    )
-    username = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="non")
-
-    groups = models.ManyToManyField(Group, related_name="custom_user_groups", blank=True)
-    user_permissions = models.ManyToManyField(Permission, related_name="custom_user_permissions", blank=True)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["role"]
-
-    def __str__(self):
-        return f"{self.email} ({self.role})"
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="profiles",blank=True)
-    full_name = models.CharField(max_length=255)
-    mobile = models.CharField(max_length=255)
-    
-    def __str__(self):
-        return self.user.username
-    def save(self, *args, **kwargs):
-        if not self.full_name:
-            self.full_name = self.user.username
-        super(Profile, self).save(*args, **kwargs)
     
 
 class Category(models.Model):
