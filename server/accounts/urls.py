231defb362794d81b3bd6e9b8f2da750f890b6e8
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2025-04-08 22:33:51 +0300

    adding email varyfying urls

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterUserView
+from .views import *
 
 app_name= 'api'
 
 urlpatterns = [
     path('', RegisterUserView.as_view(), name="register"),
+    path('varify-email/',VerifyUserEmail.as_view(), name='verify'),
 ]
