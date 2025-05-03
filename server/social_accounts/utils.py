dc78301e7aebb1e638c1ce7ea7fd2b6370c5e3ee
Bradiepoa <bradiepoa7@gmail.com>
2025-05-03 08:03:10 -0700

    authentication

--- server/social_accounts/utils.py
+++ server/social_accounts/utils.py
@@ -4,3 +4,14 @@ from django.contrib.auth import authenticate
 from django.conf import settings
 from rest_framework.exceptions import AuthenticationFailed
 
+
+class Google():
+    @staticmethod
+    def validate(access_token):
+        try:
+            id_info=id_token.varify_oauth2_token(access_token(access_token, requests.Request))
+            if "accounts.google.com" in id_info['iss']:
+                return id_info
+
+        except Exception as e:
+            return "token is invalide or expired"
