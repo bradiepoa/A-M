4b4b5a4e0a38e4c6818fa72be52d788fad61dc2a
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2025-05-06 00:07:35 +0300

    adding tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import Signup from './Signup'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Signup />} />
-      </Routes>
-    </Router>
-  )
+   <div className='App'>
+    Frontend
+   </div>
+  );
 }
 
-export default App
+export default App;
