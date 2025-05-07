d3a4c313592c8eeabcea8519e2baf54c35256c82
Bradiepoa <bradiepoa7@gmail.com>
2025-05-08 00:05:16 +0300

    worked tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 
 function App() {
- 
   return (
-   <div className='text-red-900'>
-    Frontend
-   </div>
+    <div className="App">
+      Frontend ass
+    </div>
   );
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,18 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: [],
   theme: {
-    extend: {
-      colors : {
-        "primary-200" : "#ffbf00",
-        "primary-100" : "#ffc929",
-        "secondary-200" : "#00b050",
-        "secondary-100" : "#0b1a78"
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
